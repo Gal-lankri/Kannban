aacc71e398b5e16fa3866bab11cebda0cc1b25af
Gal Lankri <gal.lankri2@gmail.com>
2023-02-12 10:39:59 +0200

    Fixing bug in backend

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 module.exports = {
-  dbURL: 'mongodb+srv://AmirGalYadid:KanbaN@cluster0.qmzv1ed.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: `mongodb+srv://${process.env.DB_KEY}@cluster0.qmzv1ed.mongodb.net/?retryWrites=true&w=majority`,
   dbName : 'board_db'
 }
